5b567eb421deb1b1fd17a7c7d33abccdc9e245dd
BlueWood1425 <nika.konyk@gmail.com>
2022-08-31 19:21:31 +0300

    Add temperature

--- index.js
+++ index.js
@@ -19,6 +19,52 @@ function formatDate(date) {
   let time = document.querySelector(`#time`);
   time.innerHTML = ` ${currentDay} ${hours} : ${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2 card mini" style="height: 150px; width: 150px; margin-left: 30px;">
+          <h3>
+            ${formatDay(forecastDay.dt)}
+            <div class="mini">
+              <img 
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt="iconWeather"
+              id="icon"
+              class="picture, smallPicture"
+            >
+            </div>
+            <span class="weather-forecast-temperatur-max">${Math.round(
+              forecastDay.temp.max
+            )}</span>
+            /
+            <span class="weather-forecast-temperatur-min">${Math.round(
+              forecastDay.temp.min
+            )}</span>
+          </h3>
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function temp(response) {
   let location = document.querySelector(`.location`);
   let temperatureL = document.querySelector(`.temperatyreL`);
@@ -89,6 +135,12 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 //////////////////////////////////////
 
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=f4db6aa7e5cfe4049fcbfe96c0455ba9&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#math").innerHTML = Math.round(
@@ -102,6 +154,8 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -120,3 +174,4 @@ let searchingForm = document.querySelector("#searching");
 searchingForm.addEventListener("submit", handleSubmit);
 
 searchCity("Kyiv");
+displayForecast();
